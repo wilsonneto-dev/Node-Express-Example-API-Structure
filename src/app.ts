022acea93b741d89b0b5741410c716c8ab3b5312
Wilson Neto <wn_br@hotmail.com>
2022-08-27 13:00:51 -0300

    feat: Add prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ ok: true });
+  res.send({
+    ok: true,
+  });
 });
 
 app.listen(3001, () => console.log("listenning..."));
